f538651283a6a002149d78fe046fba7470a840e3
zouhairagasmi <Zouhaira.gasmi@esprit.tn>
2020-10-31 14:54:00 +0100

    sonar fix z

--- src/main/java/tn/esprit/spring/config/LoggingAspect.java
+++ src/main/java/tn/esprit/spring/config/LoggingAspect.java
@@ -1,15 +1,13 @@
+  
 package tn.esprit.spring.config;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 @Component
 @Aspect
