aa42d3f21aef1682c2629b25ab3ad616825146c4
zouhairagasmi <Zouhaira.gasmi@esprit.tn>
2020-11-01 14:08:43 +0100

    jenkins error fix

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -2,6 +2,7 @@
 package tn.esprit.spring;
 import static org.junit.Assert.assertNotNull;
 
+
 import static org.junit.Assert.assertNull;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
