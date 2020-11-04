f8dea1a223923bc88e1baf8ad935acf4d2dc8327
zouhairagasmi <Zouhaira.gasmi@esprit.tn>
2020-11-04 21:52:22 +0100

    .

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -130,7 +130,7 @@ public class EmployeServiceImplTest {
 	
 	/* public static Date parseDate(String date) {
 //	     try {
-	         return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+//	         return new SimpleDateFormat("yyyy-MM-dd").parse(date);
 	     } catch (ParseException e) {
 	         return null;
 	     }
